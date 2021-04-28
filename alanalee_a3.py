1fffc679b6c75c9d7ecdda8f950182fd18486139
OoL33 <alanalee@tutanota.com>
2021-04-27 23:10:37 -0400

    final

---
+++ alanalee_a3.py
@@ -0,0 +1,27 @@
+print'Let me help you pick a Bubble Tea drink flavor. Type in a number from 1-10:'
+
+result = raw_input('Enter a Number(1-10): ')
+
+print 'You should try: '
+
+if result == '1':
+    print ('Black Milk Tea')
+elif result == '2':
+    print ('Passionfruit Green Tea')
+elif result == '3':
+    print ('Thai Milk Tea')
+elif result == '4':
+    print('Honeydew Milk Tea')
+elif result == '5':
+    print('Hibiscus Green Tea')
+elif result == '6':
+    print('Brown Sugar Milk Tea')
+elif result == '7':
+    print ('Wintermelon Oolong Tea')
+elif result == '8':
+    print ('Taro Milk Tea')
+elif result == '9':
+    print ('Lychee Black Tea')
+else:
+    print('Oreo Milk Tea')
+
